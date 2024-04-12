d207738ae4fca1bed5fbbab3c48ec6481e2123fa
aishatrue <140340401+aishatrue@users.noreply.github.com>
2024-04-12 20:32:37 +0900

    recoding

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,6 +6,7 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,73 +20,72 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @WebServlet(name = "frontControllerServletV5",urlPatterns = "/front-controller/v5/*")
 public class FrontControllerServletV5 extends HttpServlet {
 
 
-    private final Map<String, Object> handlerMappingMap = new HashMap<>();
-    private final List<MyHandlerAdapter> handlerAdapterList = new ArrayList<>();
+    private final List<MyHandlerAdapter> adapterList = new ArrayList<>();
+    private final Map<String,Object> handlerMappingMap = new HashMap<>();
+
+
+
     public FrontControllerServletV5() {
-        initHandlerMappingMap();
-        initHandlerAdapters();
+        initHandler();
+        addAdapterList();
 
 
     }
 
-    private void initHandlerAdapters() {
-        handlerAdapterList.add(new ControllerV3HandlerAdapter());
+    public void addAdapterList(){
+        adapterList.add(new ControllerV3HandlerAdapter());
     }
 
-    private void initHandlerMappingMap() {
-        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
-        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
-        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+    private void initHandler() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new
+                MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new
+                MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new
+                MemberListControllerV3());
     }
+
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV5.service");
 
-        Object handler = getHandler(request);
+        String requestURI = request.getRequestURI();
+
+        Object handler = handlerMappingMap.get(requestURI);
 
         if(handler==null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }else{
 
-            MyHandlerAdapter adapter = getHandlerAdapter(handler);
-            ModelView mv = adapter.handle(request, response, handler);
-
-
-            //request로 받아서 저장한 map을 modelview의 model에 저장또는 등등.
-            String viewName = mv.getViewName();
-            MyView view = viewResolver(viewName);
-
-            //최종 렌더링을 하는곳.
-            view.render(mv.getModel(),request,response);
+            ModelView modelView = getHandlerAdapter(request, response, handler);
+            MyView myView = viewResolver(modelView.getViewName());
+            myView.render(modelView.getModel(),request,response);
 
 
         }
+
     }
 
-    private MyHandlerAdapter getHandlerAdapter(Object handler) {
-        for (MyHandlerAdapter adapter : handlerAdapterList) {
+    private ModelView getHandlerAdapter(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        for (MyHandlerAdapter adapter : adapterList) {
             if(adapter.supports(handler)){
-                return adapter;
+               return adapter.handle(request, response, handler);
             }
-
         }
-        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler="+handler);
-
+        throw new IllegalArgumentException("존재하지 않는 어댑터입니다.");
     }
 
-    private Object getHandler(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        return handlerMappingMap.get(requestURI);
-    }
     private static MyView viewResolver(String viewName) {
         MyView myView = new MyView("/WEB-INF/views/" + viewName + ".jsp");
         return myView;
     }
+
 
 }
--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -1,7 +1,10 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
+import com.sun.jdi.VMOutOfMemoryException;
 import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,18 +17,33 @@ import java.util.Map;
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
-        //컨트롤러 v3인터페이스를 구현한게 넘어오면, true를 반환.
-        return (handler instanceof ControllerV3);
+
+        // 이 핸들러가 controllerv3의 인스턴스인지 확인해, 해당하는 어댑터가 있는지 확인하는것.
+       return (handler instanceof ControllerV3);
+
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-       //supports에서 걸러진 애로 캐스팅을 하기 때문에 괜찮음.
-        ControllerV3 controller = (ControllerV3) handler;
+
+
+        System.out.println("ControllerV3HandlerAdapter.handle");
+        //model에 저장할 데이터를 request를 통해 가져와, map에 저장.
 
         Map<String,String> paramMap = createParamMap(request);
-        ModelView modelview = controller.process(paramMap);
-        return modelview;
+
+        ControllerV3 controllerV3 = (ControllerV3) handler;
+
+
+
+            //request로 받아서 저장한 map을 modelview의 model에 저장또는 등등.
+        ModelView modelView = controllerV3.process(paramMap);
+
+       return modelView;
+
+
+
+
     }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
