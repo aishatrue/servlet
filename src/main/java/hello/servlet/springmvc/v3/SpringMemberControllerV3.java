a81b78f4f7d79acfc0a17ec7f19bb44e0510dbfc
aishatrue <140340401+aishatrue@users.noreply.github.com>
2024-04-16 00:18:27 +0900

    method mapping2

--- src/main/java/hello/servlet/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/springmvc/v3/SpringMemberControllerV3.java
@@ -6,9 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,12 +16,14 @@ import java.util.List;
 public class SpringMemberControllerV3 {
     private MemoryMemberRepository memberRepository = MemoryMemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form",method = RequestMethod.GET)
+//    @RequestMapping(value = "/new-form",method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newform(){
         return "new-form"; //ModelAndView는 모델과 뷰 정보를 담아서 반환.
     }
 
-    @RequestMapping(value = "/save",method = RequestMethod.POST)
+//    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(@RequestParam("username")String username,
                        @RequestParam("age")int age,
                        Model model
@@ -43,7 +43,8 @@ public class SpringMemberControllerV3 {
 
     // springmvc/v2/members/members
     //여기 /members를 하면 위에서 내려온 주소+/members임.
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+//    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @GetMapping("/list")
     public String membersList(Model model) {
         List<Member> memberList = memberRepository.findAll();
 
